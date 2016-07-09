9c0c150cbc1be961a9ff106281e320f11a53dc4a
ivandeepocean <ivanlouiscamus@gmail.com>
2016-07-09 15:14:40 +0800

    Notes test

--- djnotes/notes/models.py
+++ djnotes/notes/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Note(models.Model):
     title=models.CharField(max_length=512, blank = True)
     note = models.TextField(blank=True)
+    notetest = models.TextField(blank=True)
 
     def _str_(self):
         return self.title
