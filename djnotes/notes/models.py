5b70d09861bf8046fea54b30f47d6bf70cbb95c1
ivandeepocean <ivanlouiscamus@gmail.com>
2016-07-09 15:09:49 +0800

    initial commit

---
+++ djnotes/notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    title=models.CharField(max_length=512, blank = True)
+    note = models.TextField(blank=True)
+
+    def _str_(self):
+        return self.title
